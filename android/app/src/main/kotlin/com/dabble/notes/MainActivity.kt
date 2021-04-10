c3ae69d6e73fcdca7417711713908a8b6285fd4a
darryldjohnson <darryl.johnson.w@gmail.com>
2021-04-10 14:14:31 -0500

    Web

--- android/app/src/main/kotlin/com/dabble/notes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dabble.notes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screens/components/error_snack_bar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-showErrorSnackBar(BuildContext context, error) {
-  return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text("Error: $error"),
-    ),
-  );
-}
